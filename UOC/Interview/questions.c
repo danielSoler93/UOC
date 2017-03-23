7c276b888a76ef55233e0d7feb9493416e9c4195
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2017-03-23 15:13:15 +0100

    Usual Interview questions from low to high level

---
+++ UOC/Interview/questions.c
@@ -0,0 +1,47 @@
+/**************************************************************
+*
+** File:        Example c
+** Author:      Daniel Soler (www.github.com/miniaoshi)
+** Date:        18-03-2017
+** Description:
+*   """
+*
+*   What is the equivalent code of the following statement in WHILE LOOP format?  
+*   for (a=1; a&lt;=100; a++) 
+*   printf (&quot;%d\n&quot;, a * a);
+*   """
+*
+*****************************************************************/
+
+/* System header files */
+#include <stdio.h>
+#include <ctype.h>
+
+/*Application headers file*/
+
+
+/*Symboli constants*/
+
+/*User defined types*/
+
+
+/*Pre-declaration of actions and functions*/
+def forToWhile(){
+    int a = 1;
+    while (a<100){
+        printf (&quot;%d\n&quot;, a * a);
+        a++;
+    }
+
+}
+
+
+int main(int argc, char **argv)
+{
+
+
+return 0;
+
+}
+
+/* Implementation of actions and functions*/
