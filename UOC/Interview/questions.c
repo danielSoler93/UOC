a0e4725033d8f36220ebcbebf3c0b9cd9feeb392
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2017-03-23 15:51:00 +0100

    retrieve sequence implemented by adding number while multiplying by 10

--- UOC/Interview/questions.c
+++ UOC/Interview/questions.c
@@ -6,9 +6,7 @@
 ** Description:
 *   """
 *
-*   What is the equivalent code of the following statement in WHILE LOOP format?  
-*   for (a=1; a&lt;=100; a++) 
-*   printf (&quot;%d\n&quot;, a * a);
+
 *   """
 *
 *****************************************************************/
@@ -27,19 +25,50 @@
 
 /*Pre-declaration of actions and functions*/
 void forToWhile(){
+
+    /*
+    Get the equivalent code of the following for statement in WHILE LOOP format: 
+    for (a=1; a&lt;=100; a++) 
+    printf (&quot;%d\n&quot;, a * a);
+    */
+
     int a = 1;
     while (a<100){
         printf ("&quot;%d\n&quot;", a * a);
         a++;
     }
-
 }
+
+void retrieveSequence(){
+    
+    /*
+    Func to Retrieve next pattern:
+    1
+    12
+    123
+    1234
+    12345
+    */
+
+    int i = 0;
+    int Pow = 10;
+    int Newsequence = 0;
+    for (i=1; i<6; i++){
+        Newsequence = (Newsequence * Pow + i); 
+        printf("%d\n", Newsequence);
+    }
+}
+
+
 
 
 int main(int argc, char **argv)
 {
+//Question to run:
+retrieveSequence();
+}
+}
 
-forToWhile();
 return 0;
 
 }
