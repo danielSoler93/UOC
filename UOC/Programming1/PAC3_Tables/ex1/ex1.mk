49c5e3e8394f9c98124c53ca53f45ec81482c2ac
miniaoshi <daniel.soler@e-campus.uab.cat>
2017-04-26 23:34:23 +0200

    Initializing Workspace PAC3

---
+++ UOC/Programming1/PAC3_Tables/ex1/ex1.mk
@@ -0,0 +1,99 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Release
+ProjectName            :=ex1
+ConfigurationName      :=Release
+WorkspacePath          :=C:/Users/Daniel/Documents/PAC3_Tables
+ProjectPath            :=C:/Users/Daniel/Documents/PAC3_Tables/ex1
+IntermediateDirectory  :=./Release
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Daniel
+Date                   :=26/04/2017
+CodeLitePath           :=C:/CodeLite
+LinkerName             :=gcc
+SharedObjectLinkerName :=gcc -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.o.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E 
+ObjectsFileList        :="ex1.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=windres
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := ar rcus
+CXX      := gcc
+CC       := gcc
+CXXFLAGS :=  -O2 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Srcs=main.c ex1.c 
+
+Objects0=$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IntermediateDirectory)/ex1.c$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "./Release"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "./Release"
+
+PreBuild:
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Release/
+
+
---
+++ UOC/Programming1/PAC3_Tables/ex1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "ex.h"
+
+int main(int argc, char **argv)
+{	
+	tDiscount discounts[];
+	tDiscount discount1;
+	tDiscount discount2;
+	tDiscount discount3;
+	tDiscount discount4;
+	
+	printf("%d", 1);
+	discount1.dni = 1234;
+	discount2.dni = 1222;
+	discount3.dni = 1121;
+	discount4.dni = 1110;
+	
+	discounts[0] = discount1;
+	discounts[1] = discount2;
+	discounts[2] = discount3;
+	discounts[3] = discount4;
+	
+	sortDiscountsShop(discounts);
+	
+	return 0;
+}
